35f003e0594720ede7163167413cd16f51a7e520
DeveloopClick <develoop.click@gmail.com>
2023-05-02 12:49:42 +0300

    2

---
+++ cmake-build-debug/tests/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/g/My Drive/Academy/Semester-D/OS/EX2/cmake-build-debug/tests/googletest-src
+# Build directory: /mnt/g/My Drive/Academy/Semester-D/OS/EX2/cmake-build-debug/tests/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
